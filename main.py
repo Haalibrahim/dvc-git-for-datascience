24515fd837b20b066770dc70d16c8d93a296f5e1
haalibrahim <haalibrahim@gmail.com>
2022-10-30 00:48:44 -0400

    this is the second version

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 def say_hello(name):
 	print(f"Hello {name}, the future data scientist")
 
+def greetings(name):
+	print("This is a greeting from {}".format(name))
+
 say_hello("Hamad")
+greetings("Hamad")
